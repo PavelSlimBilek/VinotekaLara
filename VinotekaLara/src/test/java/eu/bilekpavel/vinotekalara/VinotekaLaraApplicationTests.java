f356450bb56a50029cd10731bf7ed10326e59203
PavelSlimBilek <bilekpavel.p@gmail.com>
2024-07-22 17:45:47 +0200

    Initial commit
    Project created

---
+++ VinotekaLara/src/test/java/eu/bilekpavel/vinotekalara/VinotekaLaraApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.bilekpavel.vinotekalara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinotekaLaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
