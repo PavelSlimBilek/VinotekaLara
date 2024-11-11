5ff2bc85bb61e560df0b4c9d7e2b5079ea4d6f9c
PavelSlimBilek <bilekpavel.p@gmail.com>
2024-11-11 12:38:30 +0100

    Add news entity

---
+++ VinotekaLara/src/main/java/eu/bilekpavel/vinotekalara/news/News.java
@@ -0,0 +1,21 @@
+package eu.bilekpavel.vinotekalara.news;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public final class News {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Setter
+    private String content;
+}
